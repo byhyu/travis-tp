e8a294cb3912960a7149d76da51b39fbd36b92d6
Hong on MakerMachine <hongyu.us@gmail.com>
2020-12-22 22:03:40 -0800

    update prioritization.py

--- src/prioritization.py
+++ src/prioritization.py
@@ -2,7 +2,7 @@ import numpy as np
 from collections import deque
 import pandas as pd
 from datetime import datetime, timedelta
-
+from tqdm import tqdm
 #%%
 
 
@@ -61,13 +61,14 @@ def _helper(post_queue_df, We, Wf, test_run_history):
     high_priority_tests = pd.DataFrame([])
     low_priorty_tests = pd.DataFrame([])
 
-    for i,test_name in enumerate(post_queue_df['test_suite'].values):
+    for i,test_name in tqdm(enumerate(post_queue_df['test_suite'].values)):
         is_new = (test_name in existing_tests)
+        print(f'i={i}, tesname:{test_name}')
         if (test_run_history is None) or (len(test_run_history) == 0) or ('test_suites' not in test_run_history.columns):
-            test_run_history = None
-
-        if (test_run_history is None ) or (len(test_run_history) == 0):
             this_test_history = None
+
+        # if (test_run_history is None ) or (len(test_run_history) == 0):
+        #     this_test_history = None
         else:
             this_test_history = test_run_history[test_run_history['test_suites'] == test_name]
 
@@ -87,16 +88,18 @@ def run_window_based_prioritization(
                                 Wp):
 
     # sort dataset by job start time
-    dataset.set_index('job_start_time')
+    dataset.set_index('job_start_time', inplace=True)
     dataset.index = pd.DatetimeIndex(dataset.index)
     dataset.sort_index(inplace=True)
 
     end_time = dataset.index[-1]
     test_time = dataset.index[0]
-    post_queue_time = test_time + timedelta(hours=Wp)
+    print(f'start time: {test_time}')
+    print(f'end time: {end_time}')
+    # post_queue_time = test_time + timedelta(hours=Wp)
 
     test_run_history = pd.DataFrame([])
-    existing_tests = set()
+    # existing_tests = set()
 
     while test_time <= end_time:
         post_queue_time = test_time + timedelta(hours=Wp)
@@ -105,62 +108,35 @@ def run_window_based_prioritization(
         sorted_test_suites = _helper(post_queue_df, We, Wf, test_run_history)
         test_run_history = test_run_history.append(sorted_test_suites)
         test_time = post_queue_time
+        print(f'test time: {test_time}')
 
     return test_run_history
-    #
-    #
-    #
-    # exec_start = test_timestamp - timedelta(hours=We)
-    # fail_start = test_timestamp - timedelta(hours=Wf)
-    # Wf_cond = any(test_run_history[fail_start:test_timestamp].build_status == 'FAIL')
-    # We_cond = test_run_history[exec_start:test_timestamp].shape[0] == 0
-    #
-    #
-    #
-    #
-    # # start time stamp
-    # test_time_str = dataset.iloc[0, :]['job_start_time']
-    # test_timestamp = datetime.strptime(test_time_str, '%Y-%m-%d %H:%M:%S.%f')
-    # post_queue_timestamp = test_timestamp
-    #
-    # # accumulated_timestamp =
-    # existing_tests = set()
-    # test_run_history = pd.DataFrame([])
-    #
-    # for test_ind in range(dataset.shape[0]):
-    #     test_row = dataset.iloc[test_ind, :]
-    #     test_name = test_row.test_suite
-    #     test_timestamp = test_row.index
-    #     # print(f'test name:{test_name}')
-    #
-    #     test_time_str = dataset.iloc[test_ind, :]['job_start_time']
-    #     test_timestamp = datetime.strptime(test_time_str, '%Y-%m-%d %H:%M:%S.%f')
-    #     end_timestamp =
-    #     while test_timestamp <= end_timestamp
-    #
-    #     if test_timestamp >= post_queue_timestamp: # trigger condition
-    #         post_queue_timestamp = test_timestamp+timedelta(hours=Wp)
-    #         post_queue = dataset.loc[test_timestamp:post_queue_timestamp]
-    #         sorted_test_suites = window_based_prioritization(post_queue, test_run_history,existing_tests, We, Wf, is_new)
-    #
-    #         test_run_history = test_run_history.append(sorted_test_suites)
-    #
-    #     post_queue_timestamp = current_timestamp + timedelta(hours=Wp)
-    #     post_queue =
-    #
-    # return 1
+
 
 #%%
 if __name__ == '__main__':
     from pathlib import Path
+    from metrics import calc_APFD
     data_dir = Path(r'C:\Users\hyu\Documents\Nutstore\Abroad2020\LOG6703')
     rails_data_file = 'cleaned_dataset_rail_100000_new_noindex.csv'
     df = pd.read_csv(data_dir / rails_data_file, sep=';')
 
-    test_run_history = run_window_based_prioritization(dataset=df,
+    # df.set_index('job_start_time', inplace=True)
+
+    test_run_history = run_window_based_prioritization(dataset=df.iloc[0:10000,:],
                                     We=24,
                                     Wf=48,
                                     Wp=12)
+    
+    #%% calc APFD
+    from metrics import calc_APFD
+    test_run_history.reset_index(inplace=True)
+    
+    failed_tests = test_run_history.index[test_run_history['build_status'] == 'failed'].tolist()
+    APFD = calc_APFD(list(range(test_run_history.shape[0])), failed_tests)
+    print(f'APFD:{APFD}')
+    
+    
 # test_run_history
 # df1 = df.set_index('job_start_time')
 # df1.index = pd.DatetimeIndex(df1.index)
