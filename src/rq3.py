438ebdcc779b40b916afa04e484049e813753340
chunliyu <chunliyu0@gmail.com>
2020-12-30 03:27:49 +0800

    add priority level of [2, 1, 0]

--- src/rq3.py
+++ src/rq3.py
@@ -3,13 +3,31 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from typing import List, Union
-from tqdm import tqdm
+#from tqdm import tqdm
 
-def is_prioritized(test_timestamp:datetime,
-                   test_run_history:pd.DataFrame,
+def update_unnecessary_patterns(all_tests_history):
+    unnecessary_patterns = pd.DataFrame([])
+    for name, group in all_tests_history.groupby(['test_suite','job_env']):
+        if (group['build_status'] == "failed").all():
+            pattern_df = pd.DataFrame([[name[0],name[1],'none']], 
+                                      columns=['test_suite', 'job_env', 'job_rvm'])
+            unnecessary_patterns = unnecessary_patterns.append(pattern_df)
+            
+    for name, group in all_tests_history.groupby(['test_suite','job_rvm']):
+        if (group['build_status'] == "failed").all():
+            pattern_df = pd.DataFrame([[name[0],'none', name[1]]], 
+                                      columns=['test_suite', 'job_env', 'job_rvm'])
+            unnecessary_patterns = unnecessary_patterns.append(pattern_df)
+    
+    return unnecessary_patterns#.drop_duplicates()
+
+
+def prioritize_test(test_row:pd.Series,
+                   test_run_history:pd.DataFrame, 
+                   existing_tests: set,
+                   unnecessary_patterns:pd.DataFrame,
                    We:Union[int, float],
-                   Wf:Union[int, float],
-                   is_new:bool):
+                   Wf:Union[int, float]):
     """
     Implementation of test selection algorithm reported in 2014 paper.
     Args:
@@ -22,16 +40,64 @@ def is_prioritized(test_timestamp:datetime,
     Returns:
         Boolean. Test case selected or not.
     """
+    
+    test_name = test_row['test_suite']
+    test_timestamp = test_row['test_suite_start_time']
+    job_env = test_row['job_env']
+    job_rvm = test_row['job_rvm']
+    build_status = test_row.build_status
+    exec_time = test_row.test_suite_duration
+    test_df = pd.DataFrame(index=[test_timestamp],
+                           data={'test_suite': [test_name], 
+                                 'job_env': [job_env],
+                                 'job_rvm': [job_rvm],
+                                 'build_status': [build_status],
+                                 'exec_time': exec_time,
+                                 'priority': [0]
+                                })
+    
+     # check if the test suite follows a certain pattern based on <GEM, rvm>
+    if unnecessary_patterns.empty:
+        return test_df
+    
+    job_env = test_row['job_env']
+    job_rvm = test_row['job_rvm']
+    
+    is_pattern1 = not unnecessary_patterns[
+        unnecessary_patterns['test_suite'] == test_name].empty #& (unnecessary_patterns['job_env'] == job_env)].empty
+    is_pattern2 = not unnecessary_patterns[
+        (unnecessary_patterns['test_suite'] == test_name)].empty #& (unnecessary_patterns['job_rvm'] == job_rvm)].empty
+    if is_pattern1 or is_pattern2: 
+        test_df['priority'] = 1
+        return test_df
+    
+    
+    # check if the current test suite is a new comer
+    if test_name not in existing_tests:
+        test_df['priority'] = 2
+        existing_tests.add(test_name)
+        return test_df
+    
+    
+    # check the We window and Wf window
     exec_start = test_timestamp - timedelta(hours=We)
     fail_start = test_timestamp - timedelta(hours=Wf)
-    Wf_cond = any(test_run_history[fail_start:test_timestamp].build_status=='FAIL')
-    We_cond = test_run_history[exec_start:test_timestamp].shape[0] == 0
-    if is_new or We_cond or Wf_cond:
-        return True
-    return False
+     
+    if not test_run_history.empty: 
+        test_run_history = test_run_history.sort_index()
+        
+        is_faulty = any(test_run_history[fail_start:test_timestamp].build_status=='failed')
+        is_idle = (test_run_history[exec_start:test_timestamp].shape[0] == 0)
+        if is_faulty or is_idle: 
+            test_df['priority'] = 2
+            return test_df
+    
+    return test_df
 
 def run_window_based_test_prioritization(wp_dataset:pd.DataFrame, 
                                          all_tests_history: pd.DataFrame,
+                                         existing_tests: set,
+                                         unnecessary_patterns:pd.DataFrame,
                                          We:Union[int, float], 
                                          Wf:Union[int, float]):
     """
@@ -45,85 +111,64 @@ def run_window_based_test_prioritization(wp_dataset:pd.DataFrame,
     """
     
     #wp_dataset.sort_values('job_start_time', inplace=True)
-    existing_tests = set()
+    
+    waiting_queue = pd.DataFrame(columns = ['test_suite', 'job_env','job_rvm','build_status','exec_time','priority'])
     accumulated_runtime = 0
-    all_tests_history = pd.DataFrame([])
     
-    prioritized_tests = pd.DataFrame([])
-    non_prioritized_tests = pd.DataFrame([])
-    
-    for index, row in tqdm(wp_dataset.iterrows()):
+    for index, row in wp_dataset.iterrows(): #tqdm(wp_dataset.iterrows()):
         test_name = row['test_suite']
-
-        test_timestamp = row['job_start_time']
-
-        if test_name in existing_tests:
-            is_new = False
-            # 第一次执行时判断all_tests_history空
-            if len(all_tests_history) == 0:
-                break
+        if(all_tests_history.empty):
+            test_history = pd.DataFrame([])
+        else:
             test_history = all_tests_history[all_tests_history['test_suite'] == test_name]
-            is_test_prioritized = is_prioritized(test_timestamp, test_history, We, Wf, is_new)
-        else:
-            is_test_prioritized = True
-            existing_tests.add(test_name)
-        
-        if is_test_prioritized:
-            build_status = row['build_status']
-            exec_time = row['test_suite_duration']
-            test_df = pd.DataFrame(index=[test_timestamp],
-                                   data={'test_suite': [test_name], 'build_status': [build_status],'exec_time': exec_time})
-            test_df.sort_index(inplace=True)
-
-            prioritized_tests = prioritized_tests.append(test_df)
-            all_tests_history = all_tests_history.append(test_df)
-        else:
-            non_prioritized_tests = non_prioritized_tests.append(test_df)
-        
-    return pd.concat([all_tests_history, non_prioritized_tests], keys=['x','y'])
-
+        test_df = prioritize_test(row, test_history, existing_tests, unnecessary_patterns, We, Wf)
+        test_df.sort_index(inplace=True)
+        waiting_queue = waiting_queue.append(test_df)
+    
+    waiting_queue.sort_values(by='priority', ascending=False, inplace=True) #by priority decending
+    return waiting_queue
 
 def split_dataset_into_wp(df:pd.DataFrame,
                           We:Union[int, float], 
                           Wf:Union[int, float],
                           Wp:Union[int, float]):
     
-    df['job_start_time'] = pd.to_datetime(df['job_start_time']) #将数据类型转换为日期类型
-    df = df.set_index('job_start_time', drop=False)
-    df = df.sort_index()
-    
+    existing_tests = set()
     all_tests_history = pd.DataFrame([])
+    unnecessary_patterns = pd.DataFrame([])
     
-    #postqueue_start_time = datetime.strptime(df.iloc[0, :]['job_start_time'], '%Y-%m-%d %H:%M:%S.%f')
-    postqueue_start_time = df.iloc[0, :]['job_start_time']
-    #postqueue_end_time = datetime.strptime(df.iloc[len(df)-1, :]['job_start_time'], '%Y-%m-%d %H:%M:%S.%f')
-    postqueue_end_time = df.iloc[len(df)-1, :]['job_start_time']
+    postqueue_start_time = df.iloc[0, :]['test_suite_start_time']
+    postqueue_end_time = df.iloc[len(df)-1, :]['test_suite_start_time']
     while postqueue_start_time < postqueue_end_time:
-        #print("in split")
         postqueue_next_start_time = postqueue_start_time + timedelta(hours=Wp)
         wp_dataset = df[postqueue_start_time:postqueue_next_start_time]  
-        #print("wp size: ",wp_dataset.shape[0])
-        processed_tests_history = run_window_based_test_prioritization(wp_dataset, all_tests_history, We=24, Wf=48)
-        #print("优化后 size: ", processed_tests_history.shape[0], "    type: ", type(processed_tests_history))
-        all_tests_history = all_tests_history.append(processed_tests_history)
-        #print("所有执行历史 size: ",all_tests_history.shape[0])
+        window_df = run_window_based_test_prioritization(wp_dataset, all_tests_history, existing_tests, unnecessary_patterns, We, Wf)        
+        all_tests_history = all_tests_history.append(window_df, ignore_index=False)
+        unnecessary_patterns = update_unnecessary_patterns(all_tests_history)
+        
         postqueue_start_time = postqueue_next_start_time
-    return all_tests_history # 按优先窗口调整的dataframe
+    
+    return all_tests_history
     
     
 if __name__ == '__main__':
     
     df = pd.read_csv("../output/cleaned_dataset_rail_100000_new_noindex.csv", sep=';', header=0)
+    df['test_suite_start_time'] = pd.to_datetime(df['test_suite_start_time']) #将数据类型转换为日期类型
+    df = df.set_index('test_suite_start_time', drop=False)
+    df = df.sort_index()
+    df = df.head(10000)
+    all_tests_history = split_dataset_into_wp(df, We=4, Wf=12, Wp=1)
     
-    all_tests_history = split_dataset_into_wp(df, We=24, Wf=48, Wp=12)
-    print(all_tests_history.shape[0])
-
-    #%%
+    print(all_tests_history.shape)
+    
+    print("priority 1: ", all_tests_history.loc[all_tests_history['priority'] == 1])
+    
+    %%
     from metrics import calc_APFD
     # APFD before prioritization
     failed_tests_before = df.index[df['build_status'] == 'failed'].tolist()
     APFD_before = calc_APFD(list(range(df.shape[0])), failed_tests_before)
-
 
     new_df = all_tests_history.reset_index() # change datetime index into integer index
     failed_tests = new_df.index[new_df['build_status'] == 'failed'].tolist()
@@ -131,4 +176,3 @@ if __name__ == '__main__':
     APFD_after = calc_APFD(list(range(new_df.shape[0])), failed_tests)
     print(f'APFD before: {APFD_before}')
     print(f'APFD after:{APFD_after}')
-    
